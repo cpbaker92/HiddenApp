3c8ee4d632ba7a74e9766f2e986ec8233f485eb0
cpbaker92 <connerparkbaker@gmail.com>
2025-07-03 23:45:37 -0500

    Fixing Screen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import WeeklyVerseScreen from '../screens/WeeklyVerseScreen';
@@ -9,14 +8,12 @@ const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Verse">
-        <Tab.Screen name="Verse" component={WeeklyVerseScreen} />
+    <Tab.Navigator initialRouteName="Verse">
+      <Tab.Screen name="Verse" component={WeeklyVerseScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="Review" component={HomeScreen} />
-        <Tab.Screen name="Add" component={HomeScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+      <Tab.Screen name="Review" component={HomeScreen} />
+      <Tab.Screen name="Add" component={HomeScreen} />
+    </Tab.Navigator>
   );
 };
 
