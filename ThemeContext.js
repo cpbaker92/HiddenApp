8f526cabb49675c66d1770e96fc90af3f865d4d3
cpbaker92 <connerparkbaker@gmail.com>
2025-07-03 22:58:13 -0500

    Update Theme

--- ThemeContext.js
+++ ThemeContext.js
@@ -19,7 +19,7 @@ export const ThemeProvider = ({ children }) => {
   const currentTheme = themeMode === 'dark' ? darkTheme : lightTheme;
 
   return (
-    <ThemeContext.Provider value={{ theme: currentTheme }}>
+    <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
